8be2466f9d3d758384805303a727e1f52cce3d38
Carlo Nucera <meditans@gmail.com>
2023-12-03 19:48:42 +0100

    Small clarification

--- 01/main.pl
+++ 01/main.pl
@@ -1,6 +1,6 @@
-% :- use_module(library(dcg_tools)).
-:- use_module('../../dcg_tools/prolog/dcg_tools').
+:- use_module(library(dcg_tools)).
 
+% If you want part 1, remove the line below
 num(N) --> {member(N-Spelling, [1-`one`, 2-`two`, 3-`three`, 4-`four`, 5-`five`, 6-`six`, 7-`seven`, 8-`eight`, 9-`nine`])}, Spelling.
 num(N) --> digit(N).
 low(N) --> lowercase(N).
@@ -8,7 +8,7 @@ low(N) --> lowercase(N).
 line(First-Last) --> non_greedy(low), num(First), (list(num | low), num(Last) | {Last = First}), list(low), nl.
 input(Lines)     --> list(line, Lines).
 
-part2(Total) :-
+part1or2(Total) :-
     once(phrase_from_file(input(Lines), 'input1.data')),
     maplist([First-Last, X] >> (X is 10*First + Last), Lines, LineResults),
     sum_list(LineResults, Total).
