9e6eaf5cd5f32bf74cd29283a574aa3d3de115fd
Carlo Nucera <meditans@gmail.com>
2023-12-06 20:34:16 +0100

    day 06

---
+++ 06/main.pl
@@ -0,0 +1,24 @@
+:- use_module(library(dcg_tools)).
+:- use_module(library(medikit)).
+:- use_module(library(clpfd)).
+
+:- dynamic race/2, big_race/2.
+
+times(Times) --> "Time:",     sp, list(natural, sp, Times).
+dists(Dists) --> "Distance:", sp, list(natural, sp, Dists).
+input(K1-K2) --> times(Times), nl, dists(Dists), nl,
+                 { maplist_op(race, Times, Dists, K1), K2 = big_race(BigTime, BigDist),
+                   atomic_list_concat(Times, BigTime0), atom_number(BigTime0, BigTime),
+                   atomic_list_concat(Dists, BigDist0), atom_number(BigDist0, BigDist) }.
+
+win(MaxTime, Record, NWays) :-
+    chain([0, PressTime, MaxTime], #=<), Distance #> Record,
+    MaxTime #= PressTime + RaceTime, Distance #= RaceTime * PressTime,
+    fd_dom(PressTime, Inf..Sup), NWays #= Sup - Inf + 1.
+
+solution(Part1, Part2) :-
+    once(phrase_from_file(input(X), 'input1.data')),
+    set_knowledge(X, [race(_,_), big_race(_,_)]),
+    aggregate_all(set(Ways), (race(T,D), win(T,D,Ways)), Res),
+    list_product(Res, Part1),
+    big_race(T, D), win(T, D, Part2).
